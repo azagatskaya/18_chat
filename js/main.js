93cc8c8485c05ef90632f35a041fa21289147262
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-05-01 02:17:33 +0300

    220501_1

--- js/main.js
+++ js/main.js
@@ -3,14 +3,17 @@
 // document.addEventListener('DOMContentLoaded', function () {
 getNameFromLS();
 
+
 function getNameFromLS() {
 	let name = localStorage.getItem('name');
 	if (name) {
 		document.querySelector('.form__name').value = name;
+		document.querySelector('.user-info__wrapper .author').innerText = name;
 	}
+	document.querySelector('.icon--active').src = getUserIconFromLS(name);
 }
 
-document.querySelector('.header .user-icon').addEventListener('click', selectIcon);
+document.querySelector('.icon--active').addEventListener('click', handleActiveIconClick);
 document.querySelector('.button--send').addEventListener('click', checkMessage);
 document.querySelector('.button--clear').addEventListener('click', clearChat);
 document.querySelector('.form__comment').addEventListener('keydown', function (e) {
@@ -20,11 +23,26 @@ document.querySelector('.form__comment').addEventListener('keydown', function (e
 	}
 });
 
+const iconToAdd = document.querySelectorAll('.user-info__select-icon .user-icon:last-child').addEventListener('click', addIconLink);
+const icons = document.querySelectorAll('.user-info__select-icon .user-icon:not(:last-child)');
+for (let icon of icons) {
+	icon.addEventListener('click', handleIconClick);
+}
+
 function checkMessage() {
 	if (!isCommentEmpty()) {
-		checkNameInLocalStorage();
+		checkNameInLS();
+		checkActiveUser();
 		sendMessage(checkSpam());
 		clearComment();
+	}
+}
+
+function checkNameInLS() {
+	let author = document.querySelector('.form__name').value;
+	let lsName = localStorage.getItem('name');
+	if ((!lsName) || (lsName !== author)) {
+		localStorage.setItem('name', author);
 	}
 }
 
@@ -44,23 +62,61 @@ function sendMessage(message) {
 	let author = document.querySelector('.form__name').value;
 	let userIcon = getUserIconFromLS(author);
 	document.querySelector('.chat').innerHTML += `<div class="chat__msg">
-	<div class="icon-wrapper">
-	<img class="user-icon" src="${userIcon}"></div>
+	<img class="user-icon" src="${userIcon}">
 	<span class="author">${author}: </span>
 	<span class="comment">${message}</span></div>`;
 }
 
 function getUserIconFromLS(author) {
 	let userIcon = localStorage.getItem(`icon${author}`);
-	return userIcon ? userIcon : 'https://cdn-icons-png.flaticon.com/512/149/149071.png';
+	return userIcon ? userIcon : 'https://cdn-icons.flaticon.com/png/512/2105/premium/2105556.png?token=exp=1651353113~hmac=6e5f91dd2ec0e342534e36a0c8cfb5a7';
 }
 
-function checkNameInLocalStorage() {
+function handleIconClick(e) {
 	let author = document.querySelector('.form__name').value;
-	let lsName = localStorage.getItem('name');
-	if ((!lsName) || (lsName !== author)) {
-		localStorage.setItem('name', author);
+	localStorage.setItem(`icon${author}`, e.target.src);
+	changeMenuDisplay();
+	changeActiveUser(e);
+}
+
+function checkActiveUser() {
+	const name = document.querySelector('.form__name').value;
+	const activeUser = document.querySelector('.user-info__wrapper .author');
+	if (activeUser.innerText !== name) {
+		activeUser.innerText = name;
+		const icon = getUserIconFromLS(name);
+		if (!!icon) {
+			document.querySelector('.icon--active').src = icon;
+		}
 	}
+}
+
+function changeActiveUser(e) {
+	const name = document.querySelector('.form__name').value;
+	const activeUser = document.querySelector('.user-info__wrapper .author');
+	if (name !== '') {
+		document.querySelector('.user-info__wrapper .author').innerText = name;
+		document.querySelector('.icon--active').src = e.target.src;
+	} else {
+		document.querySelector('.user-info__wrapper .author').innerText = 'Your name';
+	}
+}
+
+function handleActiveIconClick() {
+	changeMenuDisplay();
+
+}
+
+function addIconLink() {
+
+}
+
+function changeMenuDisplay() {
+	!isMenuDisplayed() ? document.querySelector('.user-info__select-icon').style.display = 'flex' : document.querySelector('.user-info__select-icon').style.display = 'none';
+}
+
+function isMenuDisplayed() {
+	return document.querySelector('.user-info__select-icon').style.display === 'flex';
 }
 
 function clearChat() {
